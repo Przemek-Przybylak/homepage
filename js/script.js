ac737e2be6cda6a978d3acdea0dafee55229f605
Przemyslaw Przybylak <przemyslaw.przybylak2@gmail.com>
2021-05-01 14:10:57 +0200

    functions in javascript added

--- js/script.js
+++ js/script.js
@@ -1,29 +1,37 @@
-console.log("hello!");
+{
+    const hello = () => {
+        console.log("hello!")
+    }
 
-let section__button = document.querySelector(".js-section__button");
-let headerPicture = document.querySelector(".js-section__picture");
-let pictureNone = document.querySelector(".pictureNone");
-let deleteText = ('Usuń moje zdjęcie');
-let showPicture = ("Pokaż moje zdjęcie");
+    const onsection__button = () => {
+        const section__buttonInner = document.querySelector(".js-section__buttonInner");
+        const headerPicture = document.querySelector(".js-section__picture");
+        const pictureNone = document.querySelector(".pictureNone");
+        const deleteText = ('Usuń moje zdjęcie');
+        const showPicture = ("Pokaż moje zdjęcie");
 
-section__button.addEventListener("click", () => {
-    headerPicture.classList.toggle("pictureNone");
+        headerPicture.classList.toggle("pictureNone");
+        section__buttonInner.innerText = section__buttonInner.innerText === deleteText ? showPicture : deleteText;
+    };
 
-    section__button.innerText = section__button.innerText === deleteText ? showPicture : deleteText;
+    const onmain__button = () => {
+        const secondBackground = document.querySelector(".secondBackground");
+        const main__buttonInner = document.querySelector(".js-main__buttonInner")
+        const container = document.querySelector(".container");
+        const changeButton = ("Zmień kolor tła");
+        const returnButton = ("Przywróć pierwotne tło");
 
-});
+        container.classList.toggle("secondBackground");
+        main__buttonInner.innerText = main__buttonInner.innerText === changeButton ? returnButton : changeButton;
+    }
 
-let secondBackground = document.querySelector(".secondBackground");
-let main__button = document.querySelector(".js-main__button");
-let container = document.querySelector(".container");
-let changeButton = ("Zmień kolor tła");
-let returnButton = ("Przywróć pierwotne tło");
+    const init = () => {
+        const main__button = document.querySelector(".js-main__button");
+        const section__button = document.querySelector(".js-section__button");
 
-main__button.addEventListener("click", () => {
-    container.classList.toggle("secondBackground");
-
-    main__button.innerText = main__button.innerText === changeButton ? returnButton : changeButton;
-
-
-});
-
+        hello();
+        section__button.addEventListener("click", onsection__button);
+        main__button.addEventListener("click", onmain__button);
+    }
+    init();
+}
