23ecbaef482254db38c996540e5b0d10547ef1c9
Przemek Przybylak <83243614+Przemek-Przybylak@users.noreply.github.com>
2021-08-10 09:51:53 +0200

    remove delete picture function

--- js/script.js
+++ js/script.js
@@ -1,20 +1,9 @@
 {
     const mainButton = document.querySelector(".js-main__button");
-    const sectionButton = document.querySelector(".js-section__button");
 
     const hello = () => {
         console.log("hello!")
     }
-
-    const onHidingPicture = () => {
-        const headerPicture = document.querySelector(".js-section__picture");
-        const pictureNone = document.querySelector(".pictureNone");
-        const deleteText = ('Usuń moje zdjęcie');
-        const showPicture = ("Pokaż moje zdjęcie");
-
-        headerPicture.classList.toggle("pictureNone");
-        sectionButton.innerText = sectionButton.innerText === deleteText ? showPicture : deleteText;
-    };
 
     const onBackgroundChange = () => {
         const secondBackground = document.querySelector(".secondBackground");
@@ -28,7 +17,6 @@
 
     const init = () => {
         hello();
-        sectionButton.addEventListener("click", onHidingPicture);
         mainButton.addEventListener("click", onBackgroundChange);
     }
     init();
